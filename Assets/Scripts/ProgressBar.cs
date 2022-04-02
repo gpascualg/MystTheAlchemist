02713bb02d86dc9c7a2b6702b016913663b3b2bd
CarlaMontserrat <31952505+CarlaMontserrat@users.noreply.github.com>
2022-04-02 22:49:34 +0200

    Progress bar

--- Assets/Scripts/ProgressBar.cs
+++ Assets/Scripts/ProgressBar.cs
@@ -8,15 +8,36 @@ public class ProgressBar : MonoBehaviour
 {
     public Slider LifeProgress;
     public TMP_Text TextProgress;
+    public float MaxTime;
+    public float ActualTime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        MaxTime = 300;
+    }
+
+    private void OnEnable()
+    {
+        GameManager.Instance.OnTimeChange += UpdateTime;
+    }
+
+    private void OnDisable()
+    {
+        GameManager.Instance.OnTimeChange -= UpdateTime;
+    }
+
+    public void UpdateTime(int time)
+    {
+        ActualTime = GameManager.Instance.getTime() + (float)time;
+        LifeProgress.value = (ActualTime / MaxTime);
+        TextProgress.text = (LifeProgress.value * 100).ToString("0.0") + "%";
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        float time = GameManager.Instance.getTime();
+        LifeProgress.value = (time / MaxTime);
+        TextProgress.text = (LifeProgress.value * 100).ToString("0.0") + "%";
     }
 }
